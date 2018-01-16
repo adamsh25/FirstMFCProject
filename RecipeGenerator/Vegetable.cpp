115945f1a0a1cf7d5209382f37a18ecb8da6d72d
Cheriki Adam <adam_cheriki_25@hotmail.com>
2018-01-16 23:43:39 +0200

    Major Improvemrnts

--- RecipeGenerator/Vegetable.cpp
+++ RecipeGenerator/Vegetable.cpp
@@ -1,7 +1,70 @@
 #include "stdafx.h"
 #include "Vegetable.h"
 
+IMPLEMENT_SERIAL(Vegetable, Ingredient, 1)
+
+
+
+
+Vegetable::Vegetable()
+{
+}
+
+Vegetable::Vegetable(CString _name, CString _color)
+{
+	name = _name;
+	color = _color;
+}
+
+Vegetable::Vegetable(Vegetable const & other) : Ingredient(other)
+{
+	color = other.color;
+}
+
 CString Vegetable::GetCategory() const
 {
-	return CString(L"Vegetables");
+	return CString(L"Vegetable");
+}
+
+CString Vegetable::GetColor() const
+{
+	return color;
+}
+
+
+CString Vegetable::GetName() const
+{
+	CString formmated = L"";
+	formmated.Format(L"%s %s", color, Ingredient::GetName());
+	return formmated;
+}
+
+CString Vegetable::GetImagePath() const
+{
+	return CString(L"Dairy.png");
+}
+
+int Vegetable::GetHealthScore() const
+{
+	return Ingredient::GetHealthScore() - 2;
+}
+
+void Vegetable::Serialize(CArchive & archive)
+{
+	Ingredient::Serialize(archive);
+	if (archive.IsStoring())
+	{
+		archive << color;
+	}
+	else
+	{
+		archive >> color;
+	}
+}
+
+CString Vegetable::GetInfo()
+{
+	CString formmated = L"";
+	formmated.Format(L"Dairy_%d_%s", color, Ingredient::GetInfo());
+	return formmated;
 }
--- RecipeGenerator/Vegetable.h
+++ RecipeGenerator/Vegetable.h
@@ -3,8 +3,20 @@
 class Vegetable : public Ingredient
 {
 public:
+	DECLARE_SERIAL(Vegetable)
+	Vegetable();
+	Vegetable(CString _name, CString color);
+	Vegetable(Vegetable const& other);
 	CString GetCategory() const;
-	virtual CString GetColor() const = 0;
+	virtual CString GetName() const;
+	virtual CString GetImagePath() const;
+	virtual double GetCaloriesFor100Gram() const { return 0; };
+	virtual int GetHealthScore() const;
+	virtual Vegetable* clone() const { return new Vegetable(*this); };
+	void Serialize(CArchive& archive);
+	virtual CString GetInfo();
+	virtual ~Vegetable() {};
+	virtual CString GetColor() const;
 protected:
-	const CString color;
+	CString color;
 };
